89f118ffeb3f41e82d30952d3687e496f3f648d5
Sergey Pichugov <polarizaciacd@gmail.com>
2021-01-29 17:35:42 +0300

    feat(script.js)lesson03 hard

--- script/script.js
+++ script/script.js
@@ -1,3 +1,26 @@
-
+"use strict";
 
 const num = 266219;
+
+function multi(num) {
+   let answer = 1;
+   let tmp = 0;
+
+      while (num ) {
+         tmp = num % 10;
+         num = (num - tmp) / 10;
+         answer = answer * tmp; 
+         
+      } return answer;
+} 
+
+console.log(multi(266219));
+
+
+const power = (multi(266219)) ** 3;
+console.log('power: ', power);
+
+
+
+console.log(power.toString().substring(0,2));
+
