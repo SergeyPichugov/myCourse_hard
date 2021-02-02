10c7b752997896b49f73eb6babc9da2137eee284
Sergey Pichugov <polarizaciacd@gmail.com>
2021-02-02 18:17:19 +0300

    feat(script.js): lesson04 complete

--- script/script.js
+++ script/script.js
@@ -1,48 +1,14 @@
 'use strict';
 
-let lang = 'ru';
+const arg = '    123456789012345678901234567890123456789     ';
 
-const ruCalendar = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
-      enCalendar = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
-      multiArr = new Map ([
-            ['ru', ruCalendar], 
-            ['en', enCalendar]
-         ]);
-         
+const result = function(arg) {
 
-const result = multiArr.get(lang) ? multiArr.get(lang).forEach(element => console.log(element)) :
-                        console.log('что-то пошло не так...');
-
-if (lang === 'ru') {
-   for (let i = 0; i < ruCalendar.length; i++) {
-      console.log(ruCalendar[i]);
+   if (typeof arg === 'string') {
+      (arg.length > 30) ? console.log(arg.trim().slice(0, 30) + '...') : console.log(arg.trim());
+   } else {
+      console.log('введите строку');
    }
-} else if (lang === 'en') {
-   for (let i = 0; i < enCalendar.length; i++) {
-      console.log(enCalendar[i]);
-   }
-} else {
-   console.log('что-то пошло не так...');
-}
+};
 
-
-switch (lang) {
-   case 'ru': 
-      for (let i = 0; i < ruCalendar.length; i++) {
-         console.log(ruCalendar[i]);
-      }
-      break;
-   case 'en':
-      for (let i = 0; i < enCalendar.length; i++) {
-         console.log(enCalendar[i]);
-      }
-      break;
-   default:
-      console.log('что-то пошло не так...');
-}
-
-////////////////////////////....part 2....////////////////////////
-const namePerson = 'Максим';
-
-const hello = namePerson === 'Артем' ? console.log('директор') : 
-            namePerson === 'Максим' ? console.log('преподаватель') : console.log('студент');
+result(arg);
