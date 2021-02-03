72c41d292c5d6e8afa09b498d9e8a7da80ed9ebb
Sergey Pichugov <polarizaciacd@gmail.com>
2021-02-03 19:53:20 +0300

    feat(script.js): lesson05 complete

--- script/script.js
+++ script/script.js
@@ -4,15 +4,70 @@ const isNumber = function (n) {
    return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-const arg = '    123456789012345678901234567890123456789     ';
+const arr = ['15656', '25656', '35656', '45656', '55656', '65656', '75656'];
 
-const result = function(arg) {
+function resultArr() {
+   for (let i = 0; i < arr.length; i++) {
+      let tmp = arr[i];
 
-   if (typeof arg === 'string' && !isNumber(arg)) {
-      (arg.length > 30) ? console.log(arg.trim().slice(0, 30) + '...') : console.log(arg.trim());
-   } else {
-      console.log('введите строку');
+      if (!isNumber(tmp)) {
+         console.log(tmp + ' - это не число');
+      } else if (tmp.indexOf(2) === 0 || tmp.indexOf(4) === 0) {
+         console.log(tmp);
+      }
+   }
+} 
+
+
+resultArr(arr);
+
+////////////////.....2 часть ...//////////////////// 
+
+
+let n = 100;
+
+let checkSimpl = function(n) {
+   for (let i = 2; i < n; i++) {
+      if (n % i === 0) {
+         return false;
+      }
+   }
+   return true;
+};
+
+let simplArr = function () {
+   for (let i = 2; i <=n; i++) {
+
+      if (checkSimpl(i)) {
+         console.log(i + '  Делители этого числа: 1 и ' + i);
+      }
+
    }
 };
 
-result(arg);
+simplArr();
+
+
+
+
+
+
+
+
+
+
+
+// function isSimple(n) {
+//    if (n === 1 || n === 0) {
+//       return false;
+//    } else {
+//       for (let i = 2; i < n; i++) {
+//          if (n % i === 0) {
+//             return false;
+//          }
+//       }
+//       return true;
+//    }
+// }
+
+
