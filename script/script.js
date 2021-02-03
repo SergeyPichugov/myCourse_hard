4253cf6aea09130275945b2ecec8b8689ccc9e73
Sergey Pichugov <polarizaciacd@gmail.com>
2021-02-03 16:08:19 +0300

    fix(script.js): minor fix

--- script/script.js
+++ script/script.js
@@ -1,10 +1,14 @@
 'use strict';
 
+const isNumber = function (n) {
+   return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
 const arg = '    123456789012345678901234567890123456789     ';
 
 const result = function(arg) {
 
-   if (typeof arg === 'string') {
+   if (typeof arg === 'string' && !isNumber(arg)) {
       (arg.length > 30) ? console.log(arg.trim().slice(0, 30) + '...') : console.log(arg.trim());
    } else {
       console.log('введите строку');
