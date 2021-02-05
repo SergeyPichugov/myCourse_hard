609f242ac859952f2a2664c6217dc87ca1f3a783
Sergey Pichugov <polarizaciacd@gmail.com>
2021-02-05 19:32:55 +0300

    fix(script.js): add style in js

--- script/script.js
+++ script/script.js
@@ -11,16 +11,20 @@ for (let i = 0; i < week.length; i++) {
    
    if (i === 5 || i === 6 ) {
       everyDay.className = "italic";
+      everyDay.style.fontStyle = 'italic';
    } else {
       everyDay.className = "text";
+      everyDay.style.fontSize = '20px';
    }
    
    if ((now - 1) === i) {
       everyDay.classList.add('nowDay');
+      everyDay.style.fontWeight = 'bold';
    }
 
    if (now === 0  && i === 6) {
       everyDay.classList.add('nowDay');
+      everyDay.style.fontWeight = 'bold';
    }
 
 
