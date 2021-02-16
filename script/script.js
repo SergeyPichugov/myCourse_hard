7a766c7bbfc7ea5b089866851290d9af841a7968
Sergey Pichugov <polarizaciacd@gmail.com>
2021-02-16 18:45:04 +0300

    feat(script.js):feat part 1

--- script/script.js
+++ script/script.js
@@ -1,87 +1,35 @@
 'use strict';
 
-let exactTime;
-let exactTimePartTwo;
 
-function myClock() {
-
-   let    now = new Date(),
-         weekDay =now.toLocaleString('ru', {weekday: 'long'}),
-         mounthDay = now.toLocaleString('ru', {month: 'long'}),
-         second = now.getSeconds(),
-         minute = now.getMinutes(),
-         hour = now.getHours(),
-         year = now.getFullYear(),
-         mounth = now.getMonth(),
-         dateNow = now.getDate();
-
-   weekDay = weekDay.charAt(0).toUpperCase() + weekDay.substr(1);
-
-   const formatMonth = function(tmp){
-      if (tmp.slice(-1) === 'ь' || tmp.slice(-1) === 'й') {
-         return tmp.substring(0, tmp.length - 1) + 'я';
-      } else {
-         return tmp + 'а';
+const DomElement = function (selector, height, width, bg, fontSize) {
+   this.selector = selector;
+   this.height = height;
+   this.width = width;
+   this.bg = bg;
+   this.fontSize = fontSize;
+   this.createElem = function () {
+      let elem;
+      
+      if (selector.charAt(0) === '.') {
+         elem = document.createElement('div');
+         elem.classList.add(selector.substring(1));
+         document.body.append(elem);
       }
+
+      if (selector.charAt(0) === '#') {
+         elem = document.createElement('p');
+         elem.setAttribute('id', selector.substring(1));
+         document.body.append(elem);
+      }
+      
+      elem.textContent = 'Мой новый созданный элемент';
+      elem.style.cssText = `height: ${height}; width: ${width}; background: ${bg}; font-size: ${fontSize}`;
+
    };
 
-   const formatNum = function(n, text){
-      let n1 = n % 10;
-      if ( n1 === 1){
-         return text[0];
-      } else if ( n1 > 1 && n1 < 5){
-         return text[1];
-      } else if (n > 10 && n < 20){
-         return text[2];
-      } 
-         return text[2];
-      
-   };
+};
 
-   exactTime = 'Сегодня ' + weekDay + ', ' + dateNow + ' ' + formatMonth(mounthDay) + ' ' + year + ' года, ' + hour + formatNum(hour, [' час', ' часа', ' часов']) + ' ' + minute + formatNum(minute, [' минута', ' минуты', ' минут']) + ' ' + second + formatNum(second, [' секунда', ' секунды', ' секунд']);
-
-
-      
-
-   function formatTime() {
-      second = (second < 10) ? '0' + second : second;
-      minute = (minute < 10) ? '0' + minute : minute;
-      hour = (hour < 10) ? '0' + hour : hour;
-      mounth = (mounth + 1 < 10) ? '0' + (mounth + 1) : (mounth + 1);
-      dateNow = (dateNow < 10) ? '0' + dateNow : dateNow;
-
-   }
-   formatTime();
-
-   exactTimePartTwo = dateNow + '.' + mounth + '.' + year + ' - ' + hour + ':' + minute + ':' + second;
-   
-}
-
-myClock();
-
-
-setInterval(myClock, 1000);
-
-let time = document.createElement('div');
-time.style.color = 'red';
-time.style.fontSize = '30px';
-
-
-let timePartTwo = document.createElement('div');
-timePartTwo.style.color = 'red';
-timePartTwo.style.fontSize = '30px';
-
-
-function vision(){
-   time.innerHTML = '';
-   timePartTwo.innerHTML = '';
-   
-   time.innerHTML = exactTime;
-   timePartTwo.innerHTML = exactTimePartTwo;
-   document.body.append(time);
-   document.body.append(timePartTwo);
-}
-
-setInterval(vision, 1000);
+let myElem = new DomElement('.best', '50px', '350px', 'red', '20px');
+myElem.createElem();
 
 
